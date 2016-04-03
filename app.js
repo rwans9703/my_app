2994414b3de1863e34f03356103a4482df8025f3
rwans0397 <mw97@naver.com>
2016-04-03 11:40:06 +0900

    hello

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.use(express.static(path.join(__dirname,'public')));
 // app.use(express.static(__dirname+'/public'));
 // console.log(__dirname);
 //__dirname 은 node에서 제공하는 node 파일경로를 담고있는 변수
-
+app.get('/get',function(req,res){
+res.send('hello');
+});
 app.listen(3000,function(){
   console.log('running');
 });
