3580c6e54c47bd73fbe1559e05fe87029925ee08
rwans0397 <mw97@naver.com>
2016-04-04 03:39:43 +0900

    04040339

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var path = require('path');
 var app= express();
+var mongoose = require('mongoose');
+
+mongoose.connect('process.env.MONGO_DB');
+var db = mongoose.connection;
+db.once('open',function(){
+  console.log('DB connected!');
+});
+db.on('error',function(err){
+  console.log("DB error",err);
+});
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data',dataSchema);
+Data.findOne({name:'myData'},function(err,data){
+  if(err) return console.log('Data ERROR:',err);
+  if(!data){
+    Data.create({name:'myData',count:0},function(err,data){
+        if(err) return console.log('Data ERROR:',err);
+        console.log("counter initialized:",data);
+    });
+  }
+});
 
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
-var data = {count:0};
 app.get('/',function(req,res){
-  data.count++;
-  res.render('my_first_ejs',data);
-});
+  Data.findOne({name:'myData'},function(err,data){
+    if(err) return console.log('data error:',err);
+    data.count++;
+    data.save(function(err){
+      if(err) return console.log('Data Error',err);
+      res.render('my_first_ejs',data);
+    });
+  });
+  });
 app.get('/reset',function(req,res){
   data.count=0;
   res.render('my_first_ejs',data);
