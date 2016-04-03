e16a3e77e6aaf43bf8f86df133178e8502161dfd
rwans0397 <mw97@naver.com>
2016-04-03 11:58:12 +0900

    exampleEJS

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ var express = require('express');
 var path = require('path');
 var app= express();
 
+app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
-// app.use(express.static(__dirname+'/public'));
-// console.log(__dirname);
-//__dirname 은 node에서 제공하는 node 파일경로를 담고있는 변수
-app.get('/get',function(req,res){
-res.send('hello');
+var data = {count:0};
+app.get('/',function(req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count',function(req,res){
+  if(req.query.count) data.count =req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num',function(req,res){
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
 });
 app.listen(3000,function(){
   console.log('running');
