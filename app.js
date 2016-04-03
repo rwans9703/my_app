1fe198799cfef12352da26f05e382f028363cc74
rwans0397 <mw97@naver.com>
2016-04-03 11:28:25 +0900

    04031128

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app= express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.use(express.static(__dirname+'/public'));
+// console.log(__dirname);
+//__dirname 은 node에서 제공하는 node 파일경로를 담고있는 변수
+
+app.listen(3000,function(){
+  console.log('running');
+});
